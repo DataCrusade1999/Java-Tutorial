ea89ec5c40cb57694bb09d9e973fd8d0ade08fb4
DataCrusade1999 <ashutosh.pandeyhlr007@gmail.com>
2020-11-28 12:05:30 +0530

    First Commit

---
+++ Hello_Java/src/main/java/Stunning_Apple/Hello.java
@@ -0,0 +1,10 @@
+package Stunning_Apple;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        /* A simple Hello */
+        System.out.println("Hello World");
+        
+    }
+}
---
+++ Hello_Java/src/main/java/Stunning_Apple/Variables.java
@@ -0,0 +1,28 @@
+package Stunning_Apple;
+
+import java.util.Date;
+
+public class Variables {
+    public static void main(String[] args) {
+        /*Below is the Example of Primituve Data type
+          Which is used to represent Simple types of 
+          Data like int,floats ,char,long ,double*/
+        double a=9.56,b=90,c;
+        String name = "Ashutosh Pandey";
+        c=a+b;
+        System.out.println(c);
+        System.out.println(name);
+
+        /*Below we have Example of 
+        Refrence Data type which are used to represent 
+        complex(not to be confused with mathematicle 
+        complex numbers) data types */
+
+        Date date = new Date();
+
+        System.out.println(date);
+
+        
+    }
+    
+}
