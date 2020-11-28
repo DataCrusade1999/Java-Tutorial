058a570649f33124dd438cf8867dd493b51f3873
DataCrusade1999 <ashutosh.pandeyhlr007@gmail.com>
2020-11-28 12:48:39 +0530

    First Commit

---
+++ Hello_Java/src/main/java/Stunning_Apple/Primitive_Data_Type.java
@@ -0,0 +1,26 @@
+package Stunning_Apple;
+
+public class Primitive_Data_Type {
+    public static void main(String[] args) {
+        byte Byte = -126;
+        short Short = 32_000;
+        int Integers = 23456789;
+        long Long = 234_567_89L;
+
+        float DecimalNumber = 3.14F;
+        double pi = 3.14123;
+
+        boolean is_that_right = true;
+        char Name = 'A';
+
+        System.out.println(Byte);
+        System.out.println(Short);
+        System.out.println(Integers);
+        System.out.println(Long);
+        System.out.println(is_that_right);
+        System.out.println(DecimalNumber);
+        System.out.println(pi);
+        System.out.println(Name);
+    }
+    
+}
