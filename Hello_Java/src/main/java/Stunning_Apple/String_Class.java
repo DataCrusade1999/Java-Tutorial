6b8dfbf9fac81a5506a66e68ef5e6f58049decd4
DataCrusade1999 <ashutosh.pandeyhlr007@gmail.com>
2020-11-29 13:23:01 +0530

    Sixth Commit

---
+++ Hello_Java/src/main/java/Stunning_Apple/String_Class.java
@@ -0,0 +1,19 @@
+package Stunning_Apple;
+
+public class String_Class {
+
+    public static void main(String[] args) {
+        String name =  new String("Ashutosh");
+        String fullName = new String("Ashutosh Pandey");
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.charAt(1));
+        System.out.println(name.contains("shu"));
+        System.out.println(name.equals(fullName));
+
+        
+
+
+    }
+    
+}
