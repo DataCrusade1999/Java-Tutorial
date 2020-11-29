307185662657da465756944e1a4f76f6193268fe
DataCrusade1999 <ashutosh.pandeyhlr007@gmail.com>
2020-11-29 13:52:27 +0530

    Eight Commit

---
+++ Hello_Java/src/main/java/Stunning_Apple/Arithmetic_Operation.java
@@ -0,0 +1,14 @@
+package Stunning_Apple;
+
+public class Arithmetic_Operation {
+
+    public static void main(String[] args) {
+        System.out.println(5+7);
+        System.out.println(5-7);
+        System.out.println(5%7);
+        System.out.println(5/7);
+        System.out.println(5*7);
+        System.out.println(10*(10+89)-90);
+    }
+    
+}
