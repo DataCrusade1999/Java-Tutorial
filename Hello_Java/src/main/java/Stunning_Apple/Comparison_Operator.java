de7779d7035b3f3c5141d88d17af6189dd469b61
DataCrusade1999 <ashutosh.pandeyhlr007@gmail.com>
2020-11-29 23:45:53 +0530

    Nineth Commit

---
+++ Hello_Java/src/main/java/Stunning_Apple/Comparison_Operator.java
@@ -0,0 +1,14 @@
+package Stunning_Apple;
+
+public class Comparison_Operator {
+    public static void main(String[] args) {
+        int ashu = 90;
+        int cash = 78;
+        System.out.println(ashu>cash);
+        System.out.println(ashu<cash);
+        System.out.println(ashu==cash);
+        System.out.println(ashu!=cash);
+        
+    }
+    
+}
---
+++ Hello_Java/src/main/java/Stunning_Apple/Math_Class.java
@@ -0,0 +1,10 @@
+package Stunning_Apple;
+
+public class Math_Class {
+    public static void main(String[] args) {
+        
+        System.out.println(Math.min(34, 4));
+        System.out.println( (int) Math.pow(2.0, 3.3));
+        System.out.println(Math.sqrt(25));
+    }
+}
