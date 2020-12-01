2e9ba3e256c1de72c0c399c544ef4387be659828
DataCrusade1999 <ashutosh.pandeyhlr007@gmail.com>
2020-12-01 14:00:49 +0530

    12th commit

--- Hello_Java/src/main/java/Stunning_Apple/Array.java
+++ Hello_Java/src/main/java/Stunning_Apple/Array.java
@@ -7,7 +7,19 @@ public class Array {
         int  array[]={2,3,4};
         System.out.println(Arrays.toString(array)); 
         int array_sec[]=new int [3];
+        array_sec[0]=90;
+        array_sec[1]=30;
+        array_sec[2]=900;
         System.out.println(Arrays.toString(array_sec));
+        System.out.println("The size of the array is "+array_sec.length);
+        boolean array_b[]={true,false};
+        System.out.println(Arrays.toString(array_b));
+        boolean array_c[]=new boolean [3];
+        System.out.println(Arrays.toString(array_c));
+        String array_s[]=new String [3];
+        System.out.println(Arrays.toString(array_s));
+        String array_st[]={"Ashutosh","Pandey"};
+        System.out.println(Arrays.toString(array_st));
 
 
     }
