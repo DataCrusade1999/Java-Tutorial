fefff4dd73b62b9e48c8d859eaa4002b0d4e0433
DataCrusade1999 <ashutosh.pandeyhlr007@gmail.com>
2020-12-01 15:44:58 +0530

    15th commit

---
+++ Hello_Java/src/main/java/Stunning_Apple/Scanner_class.java
@@ -0,0 +1,8 @@
+package Stunning_Apple;
+
+public class Scanner_class {
+    public static void main(String[] args) {
+        Scann
+    }
+    
+}
---
+++ Hello_Java/src/main/java/Stunning_Apple/While_loop.java
@@ -0,0 +1,19 @@
+package Stunning_Apple;
+/* The file also has do while loop */
+public class While_loop {
+    public static void main(String[] args) {
+        int i=0;
+        while (i<5) {
+            System.out.println("Ashutosh Pandey " + i);
+            i++;   
+        }
+
+
+        do {
+            System.out.println("Random Statement " + i);
+            i++;
+            
+        } while (i<=10);
+    }
+    
+}
