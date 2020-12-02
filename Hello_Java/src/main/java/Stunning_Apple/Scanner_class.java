853e701b2691376d1e6b0b8b687e006059b5bc7b
DataCrusade1999 <ashutosh.pandeyhlr007@gmail.com>
2020-12-02 16:28:52 +0530

    15th commit

--- Hello_Java/src/main/java/Stunning_Apple/Scanner_class.java
+++ Hello_Java/src/main/java/Stunning_Apple/Scanner_class.java
@@ -1,8 +1,36 @@
 package Stunning_Apple;
+import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Scanner_class {
     public static void main(String[] args) {
-        Scann
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("What is your name?");
+            String username = scanner.nextLine();
+            System.out.println("Hello " + username);
+    
+            System.out.println("How Old Are You?");
+            int age = scanner.nextInt();
+            int year = LocalDate.now().minusYears(age).getYear();
+            System.out.println("You are " + age + " years old and you were born in " + year);
+
+            if (age<18) {
+                System.out.println(username + " is not an adult");
+                
+            } else {
+                System.out.println(username + " is an adult");
+                
+            }
+
+
+        }
+        finally {
+            scanner.close();
+
+        }
+        
+
     }
     
 }
