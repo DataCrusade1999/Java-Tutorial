702492356d352eb589bc351fc56ffe2c39e10d0f
DataCrusade1999 <ashutosh.pandeyhlr007@gmail.com>
2020-12-05 11:15:58 +0530

    16th commit

---
+++ Hello_Java/src/main/java/Stunning_Apple/OOP.java
@@ -0,0 +1,17 @@
+package Stunning_Apple;
+
+public class OOP {
+    public static void main(String[] args) {
+        char letters[] = {'A','A','B','C','D','D','D'};
+        int count = countOccurence();
+
+        System.out.println(count);
+        System.out.println(letters);
+    }
+    public static int countOccurence() {
+        return 0;
+        
+    } 
+
+    
+}
